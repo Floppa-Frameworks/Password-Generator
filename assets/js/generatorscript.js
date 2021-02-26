35cc780d1340363fdeabfe82502b619fc9023180
Matt <matthewgorgees@gmail.com>
2021-02-25 23:17:00 -0500

    publish

---
+++ assets/js/generatorscript.js
@@ -0,0 +1,14 @@
+var keylisting = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWSWZ1234567890!@#$%&*/\?";
+var temporary = "";
+
+function generatePass(passwordLength) {
+    temporary = "";
+    for (var i = 0; i < passwordLength; i++) {
+        temporary += keylisting.charAt(Math.floor(Math.random() * keylisting.length))
+    }
+    return temporary;
+}
+
+function populateForm(enterLength) {
+    document.passGen.output.value = generatePass(enterLength);
+}
